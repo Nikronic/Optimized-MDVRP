0a0b9555d46f448ee472c808b1e2c8274605813b
M. Doosti Lakhani <nikan.doosti@outlook.com>
2019-09-07 21:42:24 +0430

    add Customer class
    Took 20 minutes

---
+++ utils/customer.py
@@ -0,0 +1,29 @@
+class Customer:
+    """
+    Customer class represents each node to be serviced by the vehicles.
+    These customers is going to fll `Depot` classes.
+    """
+
+    def __int__(self, id, x, y, cost, null=False):
+        """
+
+        :param id: ID assigned to node for tracking
+        :param x: X coordinate of customer
+        :param y: Y coordinate of customer
+        :param cost: The cost of servicing each customer
+        (in this project, it is 'weight' because vehicles have weight limit)
+        :param null: True if the customer is fake and used to split the list of customers as a route in each depot.
+
+        :return:
+        """
+
+        self.id = id
+        self.x = x
+        self.y = y
+        self.cost = cost
+        self.null = null
+
+    def describe(self):
+        print('ID:{}, coordinate=[{}, {}], cost={}, separator={}'.format(
+            self.id, self.x, self.y, self.cost, self.null))
+
