500ff9c052706a92f164b3de364e6a394aef5863
M. Doosti Lakhani <nikan.doosti@outlook.com>
2019-09-14 02:39:32 +0430

    add type hint and fix import

--- utils/depot.py
+++ utils/depot.py
@@ -1,18 +1,22 @@
+import numpy as np
+from typing import List
+
+from utils.customer import Customer
+
 class Depot:
     """
     Depot class represents a list of nodes assigned to this depot which we call a route.
     This class is going to be filled by `Customers` class.
     """
 
-    def __init__(self, id, x, y, capacity, depot_customers):
+    def __init__(self, id, x, y, capacity, depot_customers: List[Customer]) -> List[Customer]:
         """
-        # TODO: use type annotations for args attributes
         :param id: ID assigned to node for tracking
         :param x: X coordinate of customer
         :param y: Y coordinate of customer
         :param capacity: The maximum capacity of the Depot
         (in this project, it is filled by 'weight' of `Customers`. In other words, it indicates vehicles weight limit)
-        :param depot_customers:
+        :param depot_customers: A list of `Customer`s
 
         :return: A list of `Customers` assigned to this depot
         """
@@ -21,7 +25,7 @@ class Depot:
         self.x = x
         self.y = y
         self.capacity = capacity 
-        self.depot_customers = depot_customers 
+        self.depot_customers = depot_customers
         self.size = len(depot_customers)
 
     def describe(self, print_members=False):
